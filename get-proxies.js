05bfa01e2e10b53bf5d3e2004fd179f764b69eca
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-06-16 22:53:21 +0800

    Add get proxies module

---
+++ get-proxies.js
@@ -0,0 +1,25 @@
+const axios = require('axios')
+const fs = require('fs')
+
+async function getProxies() {
+  const proxies = await axios.get('https://api.proxyscrape.com/?request=getproxies&proxytype=http&timeout=5000&country=all&ssl=yes&anonymity=all')
+
+  fs.writeFileSync('./proxies.json', JSON.stringify(proxies.data.split("\r\n")));
+}
+
+async function getProxy() {
+    const proxies = require('./proxies.json');
+    // get random index
+    const index = Math.floor(Math.random() * proxies.length)
+    const proxy = proxies[index].split(':')
+    return [proxy[0], proxy[1]]
+}
+
+module.exports = {
+    getProxy
+};
+
+(async () => {
+    await getProxies()
+    await getProxy()
+})()
