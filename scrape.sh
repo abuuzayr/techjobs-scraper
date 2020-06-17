c249f0bcbdb2e217a5d32c3d3d9d5b929a249510
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-06-17 10:34:40 +0800

    Add logging messages to script

--- scrape.sh
+++ scrape.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
-echo date
+echo $(date -u)
+
+echo "getting proxies.."
 
 node ./get-proxies.js
 
+echo "syncing company data.."
+
 node ./sync-company-data.js > ./logs/sync-company-data
+
+echo "getting jobs from stackoverflow.."
 
 node ./stackoverflow-parser.js > ./logs/stackoverflow-parser
 
+echo "getting jobs from techinasia.."
+
 node ./techinasia-scraper.js > ./logs/techinasia-scraper
 
+echo "getting jobs from adzuna.."
+
 node ./adzuna-parser.js > ./logs/adzuna-parser
+
+echo "done!"
 
 exit 0
