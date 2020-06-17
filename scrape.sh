f7a315440af472627027b7c0a2a5bfc29998191b
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-06-17 10:42:41 +0800

    set dirpath variable

--- scrape.sh
+++ scrape.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
+dirpath=/home/abuuzayr/techjobs-scraper
 
 echo $(date -u)
 
 echo "getting proxies.."
 
-node ./get-proxies.js
+node "$dirpath/get-proxies.js"
 
 echo "syncing company data.."
 
-node ./sync-company-data.js > ./logs/sync-company-data
+node "$dirpath/sync-company-data.js" > "$dirpath/logs/sync-company-data"
 
 echo "getting jobs from stackoverflow.."
 
-node ./stackoverflow-parser.js > ./logs/stackoverflow-parser
+node "$dirpath/stackoverflow-parser.js" > "$dirpath/logs/stackoverflow-parser"
 
 echo "getting jobs from techinasia.."
 
-node ./techinasia-scraper.js > ./logs/techinasia-scraper
+node "$dirpath/techinasia-scraper.js" > "$dirpath/logs/techinasia-scraper"
 
 echo "getting jobs from adzuna.."
 
-node ./adzuna-parser.js > ./logs/adzuna-parser
+node "$dirpath/adzuna-parser.js" > "$dirpath/logs/adzuna-parser"
 
 echo "done!"
 
