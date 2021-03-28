001ae38bc99e9aa5472f0b7fe0641cc784b1c167
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-03-28 17:12:38 +0800

    Wrap axios in try catch

--- stackoverflow-parser.js
+++ stackoverflow-parser.js
@@ -57,40 +57,45 @@ async function parse() {
 
     // Save extracted items to a file.
     fs.writeFileSync('./items.json', JSON.stringify(items));
-    await Promise.all(companies.map(async co => {
-        const company = await axios.get(`${process.env.TECHJOBS_API}/api/getCompany?name=${co.name}`, {
-            validateStatus: function (status) {
-                return [200, 404].includes(status); // Resolve only if the status code is less than 500
-            }
-        })
-        if (company.status === 200 && company.data.imgUrl && company.data.tagline && !company.data.url && company.data.companySize) return
-        let imgUrl = '', tagline = '', url = '', companySize = ''
-        try {
-            // Get the company image
-            const companyPage = await axios.get(`https://stackoverflow.com/jobs/companies/${co.slug}`, {
-                headers: {
-                    'Content-Type': 'text/plain'
+    try {
+        await Promise.all(companies.map(async co => {
+            const company = await axios.get(`${process.env.TECHJOBS_API}/api/getCompany?name=${co.name}`, {
+                validateStatus: function (status) {
+                    return [200, 404].includes(status); // Resolve only if the status code is less than 500
                 }
             })
-            const $ = cheerio.load(companyPage.data);
-            imgUrl = $('#gh-logo').attr('src')
-            tagline = $('#company-name-tagline p').text()
-            url = $('a[href^="https://stackoverflow.com/jobs/companies/a/ext-link"]').attr('href')
-            companySize = $('p.fs-category:contains("Size")').next().text()
-            if (url) {
-                url = new URL(url)
-                url = url.searchParams.get('redirectUrl')
+            if (company.status === 200 && company.data.imgUrl && company.data.tagline && !company.data.url && company.data.companySize) return
+            let imgUrl = '', tagline = '', url = '', companySize = ''
+            try {
+                // Get the company image
+                const companyPage = await axios.get(`https://stackoverflow.com/jobs/companies/${co.slug}`, {
+                    headers: {
+                        'Content-Type': 'text/plain'
+                    }
+                })
+                const $ = cheerio.load(companyPage.data);
+                imgUrl = $('#gh-logo').attr('src')
+                tagline = $('#company-name-tagline p').text()
+                url = $('a[href^="https://stackoverflow.com/jobs/companies/a/ext-link"]').attr('href')
+                companySize = $('p.fs-category:contains("Size")').next().text()
+                if (url) {
+                    url = new URL(url)
+                    url = url.searchParams.get('redirectUrl')
+                }
+            } catch (e) { }
+            if (company.status === 200) {
+                const { id, ...onlyRequiredData } = company.data
+                await axios.put(`${process.env.TECHJOBS_API}/api/updateCompany`, { ...onlyRequiredData, imgUrl: imgUrl || 'https://', tagline, url, companySize })
+            } else {
+                console.log(co.name)
+                console.log(company.status)
+                await axios.post(`${process.env.TECHJOBS_API}/api/createCompany`, { name: co.name, imgUrl: imgUrl || 'https://', tagline, url, companySize })
             }
-        } catch (e) { }
-        if (company.status === 200) {
-            const { id, ...onlyRequiredData } = company.data
-            await axios.put(`${process.env.TECHJOBS_API}/api/updateCompany`, { ...onlyRequiredData, imgUrl: imgUrl || 'https://', tagline, url, companySize })
-        } else {
-            console.log(co.name)
-            console.log(company.status)
-            await axios.post(`${process.env.TECHJOBS_API}/api/createCompany`, { name: co.name, imgUrl: imgUrl || 'https://', tagline, url, companySize })
-        }
-    }))
+        }))
+    } catch (e) {
+        console.log('axios failure!')
+        console.log(e)
+    }
 
     await Promise.all(items.map(async item => {
         try {
